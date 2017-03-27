4fdacbee41ae667cec0fb9a0cdb223f5a5ce63c5
Estefanie Fernandes <estefanie@torie.com.br>
2017-03-27 00:56:03 -0300

    alteracao fb e form

---
+++ Website/include/sendemail.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once('phpmailer/PHPMailerAutoload.php');
+
+$toemails = array();
+
+$toemails[] = array(
+				'email' => 'contato@grupojolog.com.br', // Your Email Address
+				'name' => 'Grupo Jolog' // Your Name
+			);
+
+// Form Processing Messages
+$message_success = 'Sua mensagem foi enviada. Em breve, entraremos em contato ';
+
+// Add this only if you use reCaptcha with your Contact Forms
+$recaptcha_secret = ''; // Your reCaptcha Secret
+
+$mail = new PHPMailer();
+
+// If you intend you use SMTP, add your SMTP Code after this Line
+
+	$mail->IsSMTP();		// Ativar SMTP
+	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
+	$mail->SMTPAuth = true;		// Autenticação ativada
+	$mail->SMTPSecure = 'tls';	// SSL REQUERIDO pelo GMail
+	$mail->Host = '';	// SMTP utilizado
+	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
+	$mail->Username = '';
+	$mail->Password = '';
+
+
+if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	if( $_POST['template-contactform-email'] != '' ) {
+
+		$name = isset( $_POST['template-contactform-name'] ) ? $_POST['template-contactform-name'] : '';
+		$email = isset( $_POST['template-contactform-email'] ) ? $_POST['template-contactform-email'] : '';
+		$phone = isset( $_POST['template-contactform-phone'] ) ? $_POST['template-contactform-phone'] : '';		
+		$subject = isset( $_POST['template-contactform-subject'] ) ? $_POST['template-contactform-subject'] : '';
+		$message = isset( $_POST['template-contactform-message'] ) ? $_POST['template-contactform-message'] : '';
+
+		$subject = isset($subject) ? $subject : 'Nova mensagem do site';
+
+		$botcheck = $_POST['template-contactform-botcheck'];
+
+		if( $botcheck == '' ) {
+
+			$mail->SetFrom( 'contato@grupojolog.com.br' , 'Contato Virtuella' );
+			$mail->AddReplyTo( $email , $name );
+			foreach( $toemails as $toemail ) {
+				$mail->AddAddress( $toemail['email'] , $toemail['name'] );
+			}
+			$mail->Subject = $subject;
+
+			$name = isset($name) ? "Name: $name<br><br>" : '';
+			$email = isset($email) ? "Email: $email<br><br>" : '';
+			$phone = isset($phone) ? "Phone: $phone<br><br>" : '';			
+			$message = isset($message) ? "Message: $message<br><br>" : '';
+
+			$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>Formulário enviado de: ' . $_SERVER['HTTP_REFERER'] : '';
+
+			$body = "$name $email $phone $message $referrer";
+
+			// Runs only when File Field is present in the Contact Form
+			if ( isset( $_FILES['template-contactform-file'] ) && $_FILES['template-contactform-file']['error'] == UPLOAD_ERR_OK ) {
+				$mail->IsHTML(true);
+				$mail->AddAttachment( $_FILES['template-contactform-file']['tmp_name'], $_FILES['template-contactform-file']['name'] );
+			}
+
+			// Runs only when reCaptcha is present in the Contact Form
+			if( isset( $_POST['g-recaptcha-response'] ) ) {
+				$recaptcha_response = $_POST['g-recaptcha-response'];
+				$response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $recaptcha_response );
+
+				$g_response = json_decode( $response );
+
+				if ( $g_response->success !== true ) {
+					echo '{ "alert": "error", "message": "Captcha not Validated! Please Try Again." }';
+					die;
+				}
+			}
+
+			$mail->MsgHTML( $body );
+			$sendEmail = $mail->Send();
+
+			if( $sendEmail == true ):
+				echo '{ "alert": "success", "message": "' . $message_success . '" }';
+			else:
+				echo '{ "alert": "error", "message": "Email não enviado. Entre em contato através do email contato@grupojolog.com.br<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
+			endif;
+		} else {
+			echo '{ "alert": "error", "message": "Bot <strong>Detected</strong>.! Clean yourself Botster.!" }';
+		}
+	} else {
+		echo '{ "alert": "error", "message": "Please <strong>Fill up</strong> all the Fields and Try Again." }';
+	}
+} else {
+	echo '{ "alert": "error", "message": "Erro." }';
+}
+
+?>
